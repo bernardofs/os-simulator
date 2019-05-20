23bd6eac65a06bce15244993725fef05f9697adc
Daniel Santos <daniel.lopes.santoss@gmail.com>
2019-05-20 10:39:01 -0300

    Corrigir process.py

--- process.py
+++ process.py
@@ -1,12 +1,6 @@
 class Process:
 
-    def __init__(self, number, arrival_time, exec_time, deadline):
-        self.number = number
-        self.arrival_time = arrival_time
-        self.exec_time = exec_time
-        self.deadline = deadline
-
-    def __init__(self, number, arrival_time, exec_time, deadline, priority, quantum, overhead):
+    def __init__(self, number, arrival_time, exec_time, deadline, priority = 0, quantum = 0, overhead = 0):
         self.number = number
         self.arrival_time = arrival_time
         self.exec_time = exec_time
